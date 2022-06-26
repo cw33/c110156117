3eb159a2745e0678c9f7368a4469d1d9fdf380e6
cw33 <102124089+cw33@users.noreply.github.com>
2022-06-26 22:25:54 +0800

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,5 +1,14 @@
-list1=input("請輸入值為:").split(" ")
-list1.sort()
-list2=sorted(list1,reverse=True)
-print(list1)
-print(list2)
+a=input("請輸入值為:").split(",")
+c=""
+d=""
+a.sort()
+max= sorted(a, reverse = True)
+for i in a:
+    c+=i
+print(c)
+
+for i in max:
+    d+=i
+print(d)
+e=int(d)-int(c)
+print("最大值數列與最小值數列差值為:",e)
